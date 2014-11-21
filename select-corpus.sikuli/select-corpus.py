c38dbcb56770632a5cc85c87f4a4cbde1bf38d45
Farah Abbasi <farahabbasi03@gmail.com>
2014-11-20 23:15:38 -0500

    added select corpus test in firefox on ubuntu 14.4

---
+++ select-corpus.sikuli/select-corpus.py
@@ -0,0 +1,10 @@
+find("1416542452932.png")
+click("1416542464884.png")
+type("lingllama\tphoneme\n")
+
+wait("1416543208055.png")
+click("1416543208055.png")
+wait("1416543251929.png")
+wait("1416542595201.png")
+click("1416542595201.png")
+wait("1416542452932.png")
