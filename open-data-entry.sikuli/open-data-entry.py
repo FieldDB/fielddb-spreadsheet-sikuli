25702e7441e019be99e9f20d0aec8a7448c38f6d
Farah Abbasi <farahabbasi03@gmail.com>
2014-11-21 00:05:16 -0500

    using teammate tiger for tests

--- open-data-entry.sikuli/open-data-entry.py
+++ open-data-entry.sikuli/open-data-entry.py
@@ -1,6 +1,6 @@
 find("1416542452932.png")
 click("1416542464884.png")
-type("lingllama\tphoneme\n")
+type("teammatetiger\tphoneme\n")
 
 wait("1416543208055.png")
 click("1416543208055.png")
@@ -12,4 +12,3 @@ click("1416543902204.png")
 wait("arrowbuttons.png")
 wait("1416542595201.png")
 click("1416542595201.png")
-wait("1416542452932.png")
