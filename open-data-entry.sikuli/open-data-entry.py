064596010d68c2db680bd34a83e95dad5bfbdf3a
Farah Abbasi <farahabbasi03@gmail.com>
2014-11-20 23:55:31 -0500

    FIXES https://github.com/OpenSourceFieldlinguistics/FieldDB/issues/1729
    created sikuli test-firefox ubuntu 14.4

---
+++ open-data-entry.sikuli/open-data-entry.py
@@ -0,0 +1,15 @@
+find("1416542452932.png")
+click("1416542464884.png")
+type("lingllama\tphoneme\n")
+
+wait("1416543208055.png")
+click("1416543208055.png")
+wait("1416543251929.png")
+click("1416544242283.png")
+type("#DOWN#ENTER")
+
+click("1416543902204.png")
+wait("arrowbuttons.png")
+wait("1416542595201.png")
+click("1416542595201.png")
+wait("1416542452932.png")
