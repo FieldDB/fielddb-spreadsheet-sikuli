593568ec7da0b5988be9573067044581a1e1eca6
cesine <cesine@yahoo.com>
2014-12-29 18:39:45 +0700

    edit corpus is a lot of text diffs and so its a bit pikcy
    it passes on chrome, but not on safari... for https://github.com/OpenSourceFieldlinguistics/FieldDB/issues/1780

--- edit-corpus.sikuli/edit-corpus.py
+++ edit-corpus.sikuli/edit-corpus.py
@@ -2,29 +2,55 @@ find("1416542452932.png")
 doubleClick("1416542464884.png")
 wait(1)
 type("jenkins\tphoneme\n")
-
+wait(10)
 wait(Pattern("1419822974753.png").similar(0.57), 10)
 find(Pattern("1419822974753.png").similar(0.57))
 doubleClick(Pattern("1419822974753.png").similar(0.57).targetOffset(-105,2))
 wait(1)
-find(Pattern("1419823000633.png").similar(0.52))
+wait(Pattern("1419823000633.png").similar(0.52), 10)
 find(Pattern("1419821072986.png").similar(0.65))
 doubleClick(Pattern("1419821072986.png").similar(0.65))
 find("1419837965636.png")
 doubleClick("1419837965636.png")
 find("1419833172067.png")
-doubleClick(Pattern("1419833172067.png").targetOffset(-528,-143))
-type("Stories \tThis is a testing corpus.\n * With wiki formatting in the description")
+doubleClick(Pattern("1419833172067.png").targetOffset(-572,-147))
+type(Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + 
+        Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + 
+        Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + 
+        Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + 
+        Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + "Stories \t"+ 
+        Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + 
+        Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + 
+        Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + 
+        Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + 
+        Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + 
+        Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + 
+        Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + 
+        Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + 
+        Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + 
+        Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + 
+        Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + 
+        Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + "This is a testing corpus.\n * With wiki formatting in the description")
 doubleClick(Pattern("1419838668755.png").targetOffset(-177,172))
-wait(1)
+wait(20)
 find("1419833594107.png")
 doubleClick("1419833594107.png")
-find("1419838701246.png")
+wait(30)
+wait("1419846570899.png", 10)
 find(Pattern("1419838668755.png").targetOffset(-177,172))
-doubleClick(Pattern("1419838668755.png").targetOffset(-126,-145))
-type("v2.35")
-doubleClick(Pattern("1419838668755.png").targetOffset(-179,169))
-wait(2)
+doubleClick(Pattern("1419838668755.png").targetOffset(-174,-145))
+type("Testing v2.35\t" + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + 
+        Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + 
+        Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + 
+        Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + 
+        Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + 
+        Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + 
+        Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + 
+        Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + 
+        Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + 
+        Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + Key.DELETE + "This testing corpus was made at v2.35 and it contains, and potentially later versions for update verifications between versions.")
+doubleClick(Pattern("1419833172067.png").targetOffset(-562,174))
+wait(10)
 find("1416542595201.png")
 doubleClick("1416542595201.png")
 wait("1416542452932.png")
